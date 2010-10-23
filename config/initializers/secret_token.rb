24f91b8bd92828cc354080710f61664fbf584236
Michael Economy <michael@michaeleconomy.com>
2010-10-23 12:09:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blessr::Application.config.secret_token = 'd6b0e2782a91e232cadc070d205166bc5345ddda0d03ce32eb45a00765235deb7413ef255ce295c80388b2ed79036a07c32681844f33c1fde12b714f5d380cf1'
