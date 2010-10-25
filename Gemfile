e239cefc5f27eda9cabc48ace356410ae79b2037
Michael Economy <michael@michaeleconomy.com>
2010-10-24 23:14:52 -0700

    making it work !!!!(2)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.1'
 
 gem 'haml', '3.0.22'
 gem "will_paginate", "~> 3.0.pre2"
+gem 'dynamic_form', '1.1.3'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/bless_you.rb
+++ app/models/bless_you.rb
@@ -1,2 +1,13 @@
 class BlessYou < ActiveRecord::Base
+  
+  validates_format_of :to_email, :with => EMAIL_REGEXP
+  validates_length_of :your_name, :minimum => 1
+  after_create :send_mail
+  
+  def send_mail
+    BlessMailer.bless_you_email(self).deliver
+    
+    true
+  end
+  
 end
