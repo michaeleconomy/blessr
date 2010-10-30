2afba887156b85ab5f5d4b528ee00ce43f3cb234
Michael Economy <michael@michaeleconomy.com>
2010-10-30 16:41:04 -0700

    remember name
    firefox fixed

--- app/controllers/bless_yous_controller.rb
+++ app/controllers/bless_yous_controller.rb
@@ -8,6 +8,8 @@ class BlessYousController < ApplicationController
     @bless_you = BlessYou.new(params[:bless_you])
     respond_to do |format|
       if @bless_you.save
+        cookies[:your_name] =
+          { :value => @bless_you.your_name, :expires => 1.year.from_now}
         format.html do
           flash[:notice] = "eBlessYou sent to #{@bless_you.to_email}"
           redirect_to "/"
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   
   def index
-    @bless_you = BlessYou.new
+    @bless_you = BlessYou.new(:your_name => cookies[:your_name])
     @bless_yous = BlessYou.paginate :page => params[:page]
   end
 end
